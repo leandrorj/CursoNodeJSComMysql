c8745aab09fee7782857d1884f5f458066fb252b
Leandro I. Farias <leandro.i.farias@avanade.com>
2017-08-11 08:30:31 -0300

    a04 completa

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,16 +1,51 @@
-
 module.exports = function(app){
 
-  app.get('/produtos', function(req, res){
-
+  var listaProdutos = function(req, res){
     var connection = app.infra.connectionFactory();
-    var produtosBanco = new app.infra.ProdutosDAO(connection);
+    var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-    //consulta no banco
-    produtosBanco.lista(function(erros, results){
-      res.render('produtos/lista',{lista:results});
+    produtosDAO.lista(function(erros, results){
+      res.format({
+        html: function(){
+          res.render('produtos/lista', {lista:resultados});
+        },
+        json: function(){
+          res.json(resultados);
+        }
+    });
+  });
+  connection.end();
+}
+
+  app.get('/produtos', listaProdutos);
+
+  //app.get('/produtos', listaProdutos);
+  app.get('/produtos/json', function(req, res){
+    var connection = app.infra.connectionFactory();
+    var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+    produtosDAO.lista(function(err, results){
+      res.render('produtos/form');
     });
 
     connection.end();
+
+  });
+
+  app.get('/produtos/form', function(req, res){
+    res.render('produtos/form');
+  });
+
+
+  app.post('/produtos', function(req, res) {
+    var produto = req.body;
+
+    var connection = app.infra.connectionFactory();
+    var produtosDAO = new app.infra.ProdutosDAO(connection);
+    produtosDAO.salva(produto, function(erros, resultados){
+        res.redirect('/produtos');
+      });
+
+      connection.end();
   });
 }
